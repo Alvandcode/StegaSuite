6b3abb84f16628f0964dbb2a08d3ecce2649513b
Alvandcode <Alvandcode@atomicmail.io>
2026-09-02 00:00:25 +0330

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.documentfile:documentfile:1.0.1")
     implementation("androidx.core:core-ktx:1.15.0")
